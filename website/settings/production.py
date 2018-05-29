401b818866c2536778b6722cb1302b999fb30b49
Srikanth Kavuri <ksrs9312@gmail.com>
2018-05-29 22:32:11 +0530

    Added __init__.py to setting module.

--- website/settings/production.py
+++ website/settings/production.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import dj_database_url
-from .base import *
+# from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
