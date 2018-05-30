c3f10a84241e7bea3fd1e00eddbf2486a151f716
Srikanth Kavuri <ksrs9312@gmail.com>
2018-05-30 09:44:48 +0530

    Changed STATIC_ROOT.

--- website/settings/production.py
+++ website/settings/production.py
@@ -32,7 +32,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = STATICFILES_DIRS[0]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
