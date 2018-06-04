74c85e040a5674d1603d9d6228b27572fc117082
Srikanth Kavuri <ksrs9312@gmail.com>
2018-06-04 18:47:15 +0530

    Changed back to reconnecting web sockets.

--- sampleapp/consumers.py
+++ sampleapp/consumers.py
@@ -1,7 +1,7 @@
 import json, logging
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 from django.contrib.auth.models import User
 
 from .models import Message, Room
@@ -9,33 +9,36 @@ from .models import Message, Room
 log = logging.getLogger(__name__)
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-         # print(self.scope['url_route'])
-         self.room_name = self.scope['url_route']['kwargs']['u']
-         self.room_group_name = 'chat_%s' % self.room_name
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['u']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        print(self.room_group_name)
+        print(self.channel_name)
 
         # Join room group
-         await self.channel_layer.group_add(
-             self.room_group_name,
-             self.channel_name
-         )
-
-         await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-    async def receive(self, text_data):
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
+
         message = text_data_json['message']
         label = text_data_json['room_label']
         sender_username = text_data_json['sender']
 
-        await self.channel_layer.group_send(
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -45,7 +48,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    async def chat_message(self, event):
+    def chat_message(self, event):
 
         message = event['message']
         label = event['room_label']
@@ -56,14 +59,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             sender = User.objects.get(username=sender_username)
             new_msg = Message.objects.create(room=room, message=message, sender=sender)
 
-            await self.send(text_data=json.dumps({
+            self.send(text_data=json.dumps)({
                 'message': message,
                 'sender': sender_username,
                 'first_name': sender.first_name,
                 'last_name': sender.last_name,
                 'timestamp': json.dumps(new_msg.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
                                         indent=4, sort_keys=True, default=str)
-            }))
+            })
 
         except Room.DoesNotExist:
             log.debug('Room with label %s does not exist!' % label)
