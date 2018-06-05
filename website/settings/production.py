e7b033880d821fa03c2b5fb04377c162718a9dcf
Srikanth Kavuri <ksrs9312@gmail.com>
2018-06-05 14:10:39 +0530

    More changes to production settings

--- website/settings/production.py
+++ website/settings/production.py
@@ -33,7 +33,7 @@ DATABASES = {
     )
 }
 
-STATIC_URL = '/sampleapp/static/'
+STATIC_URL = '/music/sampleapp/static/'
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, 'static'),
     os.path.join(os.path.join(BASE_DIR, 'sampleapp'), 'static'),
