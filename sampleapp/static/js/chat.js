90be8077ad29f6f96b902f29051756e67082d551
Srikanth Kavuri <ksrs9312@gmail.com>
2018-06-06 14:41:30 +0530

    Changed JS file to put all code inside window.onload

--- sampleapp/static/js/chat.js
+++ sampleapp/static/js/chat.js
@@ -1,48 +1,51 @@
-let ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
-let chatSocket = new ReconnectingWebSocket(
-    ws_scheme + '://' + window.location.host +
-    '/ws/chat/' + roomName + '/');
+window.onload(function () {
+    let ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
+    let chatSocket = new ReconnectingWebSocket(
+        ws_scheme + '://' +
+        window.location.host + '/ws/chat/' + roomName + '/'
+    );
 
-chatSocket.onmessage = function(e) {
-    let data = JSON.parse(e.data),
-        message = data['message'];
-    let sender = data['sender'],
-        firstName = data['first_name'],
-        lastName = data['last_name'],
-        timestamp = JSON.parse(data['timestamp']);
+    chatSocket.onmessage = function(e) {
+        let data = JSON.parse(e.data),
+            message = data['message'];
+        let sender = data['sender'],
+            firstName = data['first_name'],
+            lastName = data['last_name'],
+            timestamp = JSON.parse(data['timestamp']);
 
-    let newDiv = document.createElement('div');
+        let newDiv = document.createElement('div');
 
-    newDiv.setAttribute('class', 'one_message ' + (sender === currentUsername) ? 'sent': 'received');
-    newDiv.innerHTML += '<p class="sender_name">' + firstName + ' ' + lastName + ' ' + timestamp + '</p>';
-    newDiv.innerHTML += '<hr>';
-    newDiv.innerHTML += '<p class="msg_text">' + message + '</p>';
+        newDiv.setAttribute('class', 'one_message ' + (sender === currentUsername) ? 'sent': 'received');
+        newDiv.innerHTML += '<p class="sender_name">' + firstName + ' ' + lastName + ' ' + timestamp + '</p>';
+        newDiv.innerHTML += '<hr>';
+        newDiv.innerHTML += '<p class="msg_text">' + message + '</p>';
 
-    document.querySelector('#chat-history').appendChild(newDiv);
-};
+        document.querySelector('#chat-history').appendChild(newDiv);
+    };
 
-chatSocket.onclose = function(e) {
-    console.error('Chat socket closed unexpectedly');
-};
+    chatSocket.onclose = function(e) {
+        console.error('Chat socket closed unexpectedly');
+    };
 
-document.querySelector('#chat-message-input').focus();
-document.querySelector('#chat-message-input').onkeyup = function(e) {
-    if (e.keyCode === 13) {  // enter, return
-        document.querySelector('#chat-message-submit').click();
-    }
-};
+    document.querySelector('#chat-message-input').focus();
+    document.querySelector('#chat-message-input').onkeyup = function(e) {
+        if (e.keyCode === 13) {  // enter, return
+            document.querySelector('#chat-message-submit').click();
+        }
+    };
 
-document.querySelector('#chat-message-submit').onclick = function(e) {
-    let messageInputDom = document.querySelector('#chat-message-input'),
-        message = messageInputDom.value,
-        roomLabel = document.querySelector('#current_room_label').getAttribute('value'),
-        sender = document.querySelector('#sender').getAttribute('value');
+    document.querySelector('#chat-message-submit').onclick = function(e) {
+        let messageInputDom = document.querySelector('#chat-message-input'),
+            message = messageInputDom.value,
+            roomLabel = document.querySelector('#current_room_label').getAttribute('value'),
+            sender = document.querySelector('#sender').getAttribute('value');
 
-    chatSocket.send(JSON.stringify({
-        'message': message,
-        'room_label': roomLabel,
-        'sender': sender
-    }));
+        chatSocket.send(JSON.stringify({
+            'message': message,
+            'room_label': roomLabel,
+            'sender': sender
+        }));
 
-    messageInputDom.value = '';
-};
+        messageInputDom.value = '';
+    };
+});
