d30b87f87938fd6364663cdd0543e47423e64196
Srikanth Kavuri <ksrs9312@gmail.com>
2018-06-07 01:55:37 +0530

    Made changes to production settings for static files.

--- website/settings/production.py
+++ website/settings/production.py
@@ -39,7 +39,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(os.path.join(BASE_DIR, 'sampleapp'), 'static'),
 ]
 
 # STATICFILES_FINDERS = (
